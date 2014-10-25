765b1aa878f6ba4bca4bb069e41319e8202c744e
王睿 <wangrui821@hotmail.com>
2014-10-25 16:03:19 +0800

    Init phase 2 pages.

---
+++ Pinecone/Controllers/Phase2Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace Pinecone.Controllers
+{
+    public class Phase2Controller : Controller
+    {
+        // GET: Phase2/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Phase2/Squirrel
+        public ActionResult Squirrel()
+        {
+            return View();
+        }
+
+        // GET: Phase2/Production
+        public ActionResult Production()
+        {
+            return View();
+        }
+
+        // GET: Phase2/Production
+        public ActionResult Person()
+        {
+            return View();
+        }
+
+        // GET: Phase2/Production
+        public ActionResult Share()
+        {
+            return View();
+        }
+    }
+}
